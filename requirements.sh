0c08a19a163b153d2abbaccae8cd8f474b51d19f
Alireza Feyzabadi <arfa79lg@gmail.com>
2024-05-29 13:01:24 +0330

    add fstab zsh docker and blutooth config

--- requirements.sh
+++ requirements.sh
@@ -38,12 +38,31 @@ git clone https://aur.archlinux.org/ventoy-bin.git
 cd ventoy-bin/
 makepkg -si
 
+#adding windows partion path to fstab 
+#guide https://askubuntu.com/questions/113733/how-to-mount-a-ntfs-partition-in-etc-fstab
+# remeber to add as the directory you have added in line blow
+sudo mkdir -p /mnt/Data
+sudo mkdir -p /mnt/windows
+sudo cp /etc/fstab /etc/fstab.bak
+
+# Change the default shell to Zsh
+chsh -s $(which zsh)
+
+#docker configuration and adding to wheel group
+sudo groupadd docker
+usermod -aG docker arfa79
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+sudo systemctl start docker.service
+sudo systemctl start containerd.service
+docker run hello-world
+
+# Bluetooth setup
+sudo systemctl start bluetooth.service 
+sudo systemctl enable bluetooth.service
+
 echo "Installation finished. Please review any additional configurations and settings as needed."
 
 # gnome extentions : gnome-shell-extensions gnome-shell-extension-dash-to-dock gnome-shell-extension-caf-feine gnome-shell-extension-persian-calendar gnome-shell-extension-blur-my-shell gnome-shell-extension-blyr gnome-shell-extension-hide-activities-button gnome-shell-extension-media-player-indicator gnome-shell-extension-openweather gnome-shell-extension-frippery-move-clock gnome-shell-extension-system-monitor gnome-shell-extension-run-command macos-mojave-gtk-theme
-# Additional configurations
-# Terminal customization with Zsh
-# Bluetooth setup
-# Adding Windows partitions
 
 # Note: Please ensure to review and modify the script according to your specific requirements before running it.
